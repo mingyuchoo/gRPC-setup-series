be728c6c6c3dcd6b6158dcb04a4528e821f1bce4
mingyuchoo <mingyuchoo@gmail.com>
2025-02-02 05:06:47 +0900

    updated

--- rust/bookstore-with-tonic/src/client.rs
+++ rust/bookstore-with-tonic/src/client.rs
@@ -1,19 +1,61 @@
 use my_book_store::book_store_client::BookStoreClient;
 use my_book_store::GetBookRequest;
+use serde::Serialize;
+use warp::Filter;
 
 pub mod my_book_store {
     tonic::include_proto!("bookstore");
 }
 
+#[derive(Serialize)]
+struct BookResponse {
+    id:     String,
+    name:   String,
+    author: String,
+    year:   i32,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = BookStoreClient::connect("http://[::1]:50051").await?;
-    let request = tonic::Request::new(GetBookRequest {
-        id: "1".into()
-    });
-    let response = client.get_book(request).await?;
+    // Define the warp filter for the GET /book route
+    let book_route =
+        warp::path!("book" / String).and_then(|id: String| fetch_book(id));
 
-    println!("RESPONSE={:?}", response);
+    // Start the warp server
+    warp::serve(book_route).run(([127, 0, 0, 1], 3030)).await;
 
     Ok(())
+}
+
+// Function to fetch book data from the proto server
+async fn fetch_book(id: String) -> Result<impl warp::Reply, warp::Rejection> {
+    let mut client = BookStoreClient::connect("http://[::1]:50051")
+        .await
+        .map_err(|e| {
+            eprintln!("Failed to connect to the server: {}", e);
+            warp::reject::not_found()
+        })?;
+    let request = tonic::Request::new(GetBookRequest {
+        id: id.clone()
+    });
+
+    let response = client.get_book(request).await.map_err(|e| {
+        eprintln!("Failed to get book: {}", e);
+        warp::reject::not_found()
+    })?;
+
+    let book = response.into_inner();
+    let book_response = BookResponse {
+        id:     book.id,
+        name:   book.name,
+        author: book.author,
+        year:   book.year,
+    };
+
+    let json = serde_json::to_string(&book_response).map_err(|e| {
+        eprintln!("Failed to serialize response: {}", e);
+        warp::reject::not_found()
+    })?;
+
+    Ok(warp::reply::html(json))
 }
